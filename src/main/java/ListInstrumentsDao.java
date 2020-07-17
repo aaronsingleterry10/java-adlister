310eb6d31539b2133971e9bfd1edc086a6737f00
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-17 16:50:24 -0500

    added code

--- src/main/java/ListInstrumentsDao.java
+++ src/main/java/ListInstrumentsDao.java
@@ -1,2 +1,27 @@
-public class ListInstrumentsDao {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInstrumentsDao implements Instruments {
+    private List<Instrument> inst;
+
+    public List<Instrument> all() {
+        if (inst == null) {
+            inst = generateInst();
+        }
+        return inst;
+    }
+
+    private List<Instrument> generateInst() {
+        List<Instrument> inst = new ArrayList<>();
+
+        inst.add(new Instrument(1, "Electric Guitar", "String Instrument", "Opens the gates of Rock"));
+
+        inst.add(new Instrument(2, "Alto Sax", "Woodwinds", "Smooth things out.."));
+
+        inst.add(new Instrument(3, "Trumpet", "Brass", "Jazz things up a bit.."));
+
+        inst.add(new Instrument(4, "Drums", "Percussion", "Lays down the groove..."));
+
+        return inst;
+    }
 }
