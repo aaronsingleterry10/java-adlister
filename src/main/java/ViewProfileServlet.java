f53dec0ba6a935a97674debad409c0961083f972
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-17 10:42:06 -0500

    pushing up

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (request.getMethod().equalsIgnoreCase("post")) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            if (username.equals("admin") && password.equals("password")) {
+                response.sendRedirect("/profile.jsp");
+            }
+        }
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
