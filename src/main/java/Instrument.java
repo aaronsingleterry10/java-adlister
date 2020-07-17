004efb1f6aea17f75ce5e9ca1561a7d9d20ed330
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-17 14:54:00 -0500

    completed instrument class

--- src/main/java/Instrument.java
+++ src/main/java/Instrument.java
@@ -1,2 +1,41 @@
 public class Instrument {
+    private int id;
+    private String name;
+    private String type;
+    private String description;
+
+    public Instrument(int id, String name, String type, String description) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getType() {
+        return this.type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return this.description;
+    }
 }
