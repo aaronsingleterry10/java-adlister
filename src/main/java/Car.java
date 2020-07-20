9c96002725945bbea5174e04ffcbaba380cd2710
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-20 11:48:52 -0500

    created car.java with properties, constructor, getters/setters

---
+++ src/main/java/Car.java
@@ -0,0 +1,50 @@
+//The Car entity should have the following:
+//  id (use a long type)
+//  make (use a string type)
+//  model (use a string type)
+//  year (use an int)
+public class Car {
+    private long id;
+    private String make;
+    private String model;
+    private int year;
+
+    public Car(long id, String make, String model, int year) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
