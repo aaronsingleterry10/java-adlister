40160f38e33599937b5f7e204e296ad78719c115
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-20 11:21:00 -0500

    created navbarLogout and refactored profile.jsp

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -1,2 +1,14 @@
-public class LogoutServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/login")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp");
+    }
 }
