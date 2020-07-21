b9841f1bc754e60b675ae3a77afc068115fcd958
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-21 15:23:46 -0500

    created store.jsp and added code to storeservlet

--- src/main/java/StoreServlet.java
+++ src/main/java/StoreServlet.java
@@ -1,2 +1,14 @@
-public class StoreServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "StoreServlet", urlPatterns = "/store")
+public class StoreServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/store/store.jsp");
+    }
 }
---
+++ src/main/webapp/WEB-INF/store/store.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: aaronsingleterry
+  Date: 7/21/20
+  Time: 3:22 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Waldo Martinez</title>
+</head>
+<body>
+    <h1>Welcome To Waldo Martinez!</h1>
+</body>
+</html>
