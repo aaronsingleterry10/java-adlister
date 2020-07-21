22d0900cdec2cf5adad938fbab12e38b49eb95f5
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-21 15:01:24 -0500

    created constructor, getters, setters

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,35 @@
 public class Item {
+    private long id;
+    private String name;
+    private int priceInCents;
+
+    public Item(long id, String name, int priceInCents) {
+        this.id = id;
+        this.name = name;
+        this.priceInCents = priceInCents;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriceInCents() {
+        return priceInCents;
+    }
+
+    public void setPriceInCents(int priceInCents) {
+        this.priceInCents = priceInCents;
+    }
 }
