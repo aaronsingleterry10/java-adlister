5b8ac5ff6c840caa86a74aeb6084dfde0d4f3a64
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-21 15:56:47 -0500

    created static method in daofactory

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -2,11 +2,19 @@ import java.sql.SQLException;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Items itemsDao;
 
     public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
             adsDao = new MySQLAdsDao(new Config());
         }
         return adsDao;
+    }
+
+    public static Items getItemsDao() throws SQLException {
+        if (itemsDao == null) {
+            itemsDao = new MySQLItemsDao(new Config());
+        }
+        return itemsDao;
     }
 }
