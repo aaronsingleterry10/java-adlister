bbc8333ef06a348a3265f3b1654e101f580381f7
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-21 15:12:32 -0500

    created seeder file and added to items table

---
+++ src/items_seeder.sql
@@ -0,0 +1,12 @@
+USE adlister_db;
+
+insert into items (name, price_in_cents) values ('Pork - Butt, Boneless', 500);
+insert into items (name, price_in_cents) values ('Cabbage - Red', 300);
+insert into items (name, price_in_cents) values ('Nut - Almond, Blanched, Ground', 400);
+insert into items (name, price_in_cents) values ('Pork - Bacon, Sliced', 2000);
+insert into items (name, price_in_cents) values ('Wonton Wrappers', 650);
+insert into items (name, price_in_cents) values ('Cheese - Goat With Herbs', 473);
+insert into items (name, price_in_cents) values ('Blueberries - Frozen', 483);
+insert into items (name, price_in_cents) values ('Dried Apple', 888);
+insert into items (name, price_in_cents) values ('Cheese - Gouda', 333);
+insert into items (name, price_in_cents) values ('Truffle Paste', 800);
