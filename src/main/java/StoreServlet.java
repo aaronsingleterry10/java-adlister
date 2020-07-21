db8c9aeb0b8ea05af84d6d23061ddadf9c75e049
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-21 15:57:59 -0500

    added to Store

--- src/main/java/StoreServlet.java
+++ src/main/java/StoreServlet.java
@@ -4,11 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "StoreServlet", urlPatterns = "/store")
 public class StoreServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/store/store.jsp");
+        try {
+            req.setAttribute("items", DaoFactory.getItemsDao().listAll());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
